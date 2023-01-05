1200bd0f469d0815eb73acfca02f0399cb72ce54
Filozop <micheilfilozop9@gmail.com>
2023-01-05 19:57:42 +0200

    самsыкс

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-"""import telebot
-from translate import Translator
-bot = telebot.TeleBot('5894648667:AAEm94P80FaYrhJE_GPNoTQa7Mgy5hL2J4A')
-translator = Translator(from_lang='ukrainian', to_lang='english')
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, f"{message.from_user.first_name},привіт,я все перекладаю ,обери мову  з якою ви хочете перекладати")
 
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message,translator.translate(message.text))
-bot.infinity_polling()"""
-"""import requests
+import requests
 from bs4 import BeautifulSoup
 URL = 'https://hdrezka24.cam/movies/1'
 
@@ -19,8 +7,9 @@ response = requests.get(URL)
 
 soup = BeautifulSoup(response.text,'lxml')
 data = soup.find('div',class_='th-item')
-name = data.find('a',class_='title="Смотреть онлайн «Аватар: Путь воды»')#######TODO
-print(name)"""
+# a = [c.text for c in data]
+link = 'https://hdrezka24.cam' + data.find('a',class_="th-in with-mask").get('href')
+print(link)
 
 
 
