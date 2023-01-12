12a985c8a8a00df7d826580e10b28ef783327038
taras-fid <taras.fid@gmail.com>
2023-01-12 02:58:03 +0200

    bot fixed.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import telebot
 
 TOKEN = '5740820565:AAF_P7ZoRpiQYFUQ3WbKYJRC5HE_r4QyMD4'
 bot = telebot.TeleBot(TOKEN)
+# TOK = '5803031167:AAGyRzVMJUWTvUl8PLEqC4CiR3pK1duXxH4'
+# bot = telebot.TeleBot(TOK)
+
+
 def parser():
     for number in range(1, 2698):
 
@@ -21,34 +25,29 @@ def parser():
             link = 'https://hdrezka24.cam' + i.find('a', class_="th-in with-mask").get('href')
 
             with open('films.txt', 'a', encoding='utf-8') as file:
-                file.write('\n' + name + ',' + year + ',' + quality + ',' + link)
-
-
-
-
-
-
+                file.write('\n' + name + '-:-' + year + '-:-' + quality + '-:-' + link)
 
 
 @bot.message_handler(commands=['start'])
 def welcome_user(message):
     bot.reply_to(message,
                  f"{message.from_user.first_name},привіт!,\nВведи назву фільму ,який хочеш подивитися:")
-    bot.register_next_step_handler(message,search_film)
-bot.infinity_polling()
+    bot.register_next_step_handler(message, search_film)
+
 
 @bot.message_handler(func=lambda message: True)
 def search_film(message):
-    if message.text == message(parser()):
-        with open('films.txt','r',encoding='utf-8') as file:
-            for i in file.readlines():
-                if message in i:
-                    return i.split(',')
-        bot.reply_to(message,'\n' + parser(name) + ',' + year + ',' + quality + ',' + link)
-    else:
-        bot.register_next_step_handler(message,error_film)
+    with open('films.txt', 'r', encoding='utf-8') as file:
+        arr = []
+        for i in file.readlines():
+            if message.text.lower() in i.lower():
+                arr = i.split('-:-')
+        if arr:
+            bot.send_message(message.chat.id, '\n' + arr[0] + ',' + arr[1] + ',' + arr[2] + ',' + arr[3])
+        else:
+            bot.send_message(message.chat.id,
+                             f"{message.from_user.first_name} Переконайтеся, що ввели правильну назву фільму,"
+                             "\nмені не вдалось нічого занйти по вашому запиту")
 
-def error_film(message):
-    bot.reply_to(message,f"{message.from_user.first_name} Переконайтеся, що ввели правильну назву фільму")
-    bot.register_next_step_handler(message, search_film)
 
+bot.infinity_polling()
